a8e9b4f41d26adeb528ce87ffa47ac696e240419
TbotInnovations <149735728+JoyzaTech@users.noreply.github.com>
2024-11-14 14:24:24 -0700

    Day 2

---
+++ src/Test.jsx
@@ -0,0 +1,12 @@
+function Test(){
+    const sports = "football"
+    return(
+        <>
+            <h1>Lucas Saner</h1>
+            <p>Hello, my name is Lucas Saner</p>
+            <h3>{sports}</h3>
+        </>
+    )
+}
+
+export default Test
